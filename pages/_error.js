eaf78720366bbf8a4a8818a033874d62e7867e1f
Mohamed Adek <mh4md.a@gmail.com>
2019-12-31 14:18:11 +0200

    Add custom button to error screen

--- pages/_error.js
+++ pages/_error.js
@@ -1,18 +1,27 @@
 import '../styles/main.css'
+import Link from 'next/link'
 
 function CustomError({ statusCode }) {
   return (
-    <figure className="text-center py-10">
-      <img
-        alt="Showing a properly cat according the status code"
-        width="40%"
-        src={`https://http.cat/${statusCode}`}
-        className="m-auto"
-      />
-      <figcaption>
-        <h1>{statusCode} page not found</h1>
-      </figcaption>
-    </figure>
+    <div className="text-center py-10">
+      <figure className="text-center mb-4">
+        <img
+          alt="Showing a properly cat according the status code"
+          width="40%"
+          src={`https://http.cat/${statusCode}`}
+          className="m-auto"
+        />
+        <figcaption>
+          <h1>{statusCode} page not found</h1>
+        </figcaption>
+      </figure>
+      <Link href="/">
+        <button className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded inline-flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 25" width="18" height="18"><path fill="white" className="heroicon-ui" d="M5.41 11H21a1 1 0 0 1 0 2H5.41l5.3 5.3a1 1 0 0 1-1.42 1.4l-7-7a1 1 0 0 1 0-1.4l7-7a1 1 0 0 1 1.42 1.4L5.4 11z"/></svg>
+          <a>Back to home</a>
+        </button>
+      </Link>
+    </div>
   );
 }
 
